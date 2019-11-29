d4972d74f784393b7418bde8ed4ff3b8a6669d91
unknown <tylerbroere@gmail.com>
2019-11-29 22:10:05 +0100

    Hey user toegevoegdn; alleen nog de content aanpassen

--- html/js/script.js
+++ html/js/script.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
 
+    $("#toggle").on("click", function () {
+        $("#hey_user").toggleClass("hidden");
+    });
+
     //  Maakte de sidenav kleiner voor mobielen (met een breedte van maximaal 768)
     if ($(window).width() < 768) {
         $("#toggle").trigger("click");
     }
+
+    //if ($('#slide-out').hasClass('slim')) {
 
 });
