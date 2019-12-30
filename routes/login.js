e62f13fd71e18ebaffa9d04733dbc1cb8b72c819
unknown <tylerbroere@gmail.com>
2019-12-30 12:10:13 +0100

    jwt token toegevoegd

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
 const {
     User,
@@ -18,34 +20,38 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res, next) => {
-    User.findOne({
+    User.find({
             email: req.body.email
         })
         .exec()
         .then(user => {
             if (user.length < 1) {
                 return res.status(401).json({
-                    message: "1E-mail or password incorrect"
+                    message: "E-mail or password incorrect"
                 });
             };
             console.log(user);
 
-            bcrypt.compare(req.body.wachtwoord, user.wachtwoord, (err, result) => {
+            bcrypt.compare(req.body.wachtwoord, user[0].wachtwoord, (err, result) => {
 
-                if (err) {
-                    return res.status(401).json({
-                        message: "2E-mail or password incorrect"
-                    });
-                };
+                if (err) return res.status(401).json({
+                    message: "E-mail or password incorrect"
+                });
 
-                if (result) {
-                    return res.status(200).json({
-                        message: "Login success"
-                    });
-                };
+                const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, config.get('jwtPrivateKey'), {
+                    expiresIn: "1h"
+                });
+
+                if (result) return res.status(200).json({
+                    message: "Login success",
+                    token: token
+                });
 
                 res.status(401).json({
-                    message: "3E-mail or password incorrect"
+                    message: "E-mail or password incorrect"
                 });
 
             });
